e9503bcf1dd6fa1ff22d9c543687847c693f49f2
dnsdev2140 <davidns2140@gmail.com>
2024-10-04 07:42:40 +0700

    delete js code

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-const menuToggle = document.querySelector('.nav__menu-toggle');
-const navLinks = document.querySelector('.nav__links');
+// const menuToggle = document.querySelector('.nav__menu-toggle');
+// const navLinks = document.querySelector('.nav__links');
 
-menuToggle.addEventListener('click', () => {
-  navLinks.style.display = navLinks.style.display === 'flex' ? 'none' : 'flex';
-});
+// menuToggle.addEventListener('click', () => {
+//   navLinks.style.display = navLinks.style.display === 'flex' ? 'none' : 'flex';
+// });
 
 console.log('test');
